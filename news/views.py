f480f6effe647baac58f985e890e0d235990979e
Mike Horrocks <michael.horrocks@hotmail.co.uk>
2019-12-22 11:06:44 +0000

    Updated news

--- news/views.py
+++ news/views.py
@@ -4,7 +4,9 @@ from .models import News
 
 def index(request):
     news_list = News.objects.order_by('-pub_date')[:9]
-    context = {'news_list': news_list}
+    context = {'news_list': news_list,
+      'foobar': 'baz'
+    }
     return render(request, 'news/index.html', context)
 
 def detail(request, slug):
